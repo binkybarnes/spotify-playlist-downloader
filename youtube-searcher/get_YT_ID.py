e22493d443ec4d471b29ef8b7d9b5682d7795ba5
binkybarnes <132546955+binkybarnes@users.noreply.github.com>
2023-09-19 15:06:22 +0800

    Step 2: finished get id for yt vid function

---
+++ youtube-searcher/get_YT_ID.py
@@ -0,0 +1,36 @@
+
+import googleapiclient.discovery
+import pprint as pprint
+
+from dotenv import load_dotenv
+import os
+import json
+
+
+def get_YtId(YT_API_KEY, vid_name):
+    load_dotenv()
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = YT_API_KEY
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+    request = youtube.search().list(
+        part="snippet",
+        maxResults=1,
+        order="relevance",
+        q=vid_name,
+        safeSearch="none"
+    )
+
+    response = request.execute()
+    return response["items"][0]["id"]["videoId"]
+
+
+load_dotenv()
+yt_ApiKey = os.getenv("YT_API_KEY")
+yt_id = get_YtId(yt_ApiKey, "CALMNNESS")
+
+print(f"https://www.youtube.com/watch?v={yt_id}")
